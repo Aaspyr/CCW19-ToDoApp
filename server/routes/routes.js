0eabcbb578df6b0cce8fd70202175dae90bde9c5
Gengar-i <rmmswoboda@gmail.com>
2019-09-11 22:03:48 +0200

    routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,3 +1,13 @@
-const dupa = () => {
-    console.log("Mateusz jest najlepszym kompanem");
+const express = require('express');
+const list = require('../models/list')
+const task = require('../models/task');
+const user = require('../models/user');
+const error = require('../models/error');
+
+module.exports = function(app) {
+  app.use(express.json());
+  app.use('/api/list', list);
+  app.use('/api/task', task);
+  app.use('/api/users', user);
+  app.use(error);
 }
