5c170bc79d77dd69013dc4ee3615d0bd26c06196
Gengar-i <rmmswoboda@gmail.com>
2019-09-11 23:06:13 +0200

    tasks and lists enjoing fun

--- server/routes/lists.js
+++ server/routes/lists.js
@@ -1,2 +1,34 @@
+const {list, validate} = require('../models/list'); //będzie supi jak Olcia zrobi <3
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const list = await List.find().sort('name');
+  res.send(list);
+});
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+  
+  res.send(list);
+});
+
+router.put('/:id', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+
+  if (!list) return res.status(404).send('The list with the given ID was not found.');
+  
+  res.send(list);
+});
+
+router.delete('/:id', async (req, res) => {
+  const list = await List.findByIdAndRemove(req.params.id);
+
+  if (!list) return res.status(404).send('The list with the given ID was not found.');
+
+  res.send(list);
+});
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,2 +1,34 @@
+const {task, validate} = require('../models/task'); //będzie supi jak Olcia zrobi <3
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const tasks = await Task.find().sort('name');
+  res.send(tasks);
+});
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  
+  res.send(task);
+});
+
+router.put('/:id', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  if (!task) return res.status(404).send('The task with the given ID was not found.');
+  
+  res.send(task);
+});
+
+router.delete('/:id', async (req, res) => {
+  const task = await Task.findByIdAndRemove(req.params.id);
+
+  if (!task) return res.status(404).send('The task with the given ID was not found.');
+
+  res.send(task);
+});
