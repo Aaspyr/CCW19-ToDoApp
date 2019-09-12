72ca0791ad14a4fddee4899020e90a55f3a76007
Weronika <riqiviki@gmail.com>
2019-09-12 14:39:13 +0200

    change name to app.js

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+//const routes = require('./routes')(app);
+
+app.get('/', (req,res) => {
+    res.send('hello');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
