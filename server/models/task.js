17eebb8bdf743f9d9b44f1a31b9cf3f90c79348a
TheManis92 <mateuszkogut007@gmail.com>
2019-09-19 01:47:49 +0200

    Add joi validation, fix errors

--- server/models/task.js
+++ server/models/task.js
@@ -1,9 +1,11 @@
+const Joi = require('joi');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const TaskSchema = new mongoose.Schema({
     userId: {
-        type: String,
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'User',
         required: true
     },
     name: {
@@ -13,14 +15,17 @@ const TaskSchema = new mongoose.Schema({
         required: true
     },
     list: {
-        type: String,
-        minlength: 1,
-        maxlength: 100,
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'List',
         required: true
     },
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    deadline: {
+        type: Date,
+        required: true
     },
     done: {
         type: Boolean,
@@ -28,4 +33,18 @@ const TaskSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model("Task", TaskSchema);
+function validateTask(task) {
+    const schema = {
+      userId: Joi.required(),
+      name: Joi.string().min(1).max(15).required(),
+      list: Joi.required(),
+      deadline: Joi.date().required(),
+      done: Joi.boolean()
+    };
+  
+    return Joi.validate(task, schema);
+}
+  
+  
+module.exports.validate = validateTask;
+module.exports.Task = mongoose.model("Task", TaskSchema);
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt');
-const User = require('../models/user');
+const {User, validate} = require('../models/user');
 const express = require('express');
 const router = express.Router();
 
@@ -17,14 +17,14 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
 
-  let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send('User already registered.');
+  const user = new User( { name: req.body.name, email: req.body.email, password: req.body.password});
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   try {
-    user = new User( { name: req.body.name, email: req.body.email, password: req.body.password});
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
     await user.save();
   } catch(err) {
     res.status(400).send(err.message);
@@ -35,21 +35,21 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const user = User.findById(req.params.id);
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = await User.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   if(!user) return res.status(404).send('The user with the given ID was not found.');
 
-  try {
-    user.name = req.body.name;
-    user.email = req.body.email;
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(req.body.password, salt);
-    await user.save();
-  } catch(err) {
-    res.status(400).send(err.message)
-  }
-
-  res.send(user);
+  res.send(user);  
 });
 
 router.delete('/:id', async (req, res) => {
