e85d129962cef55098a5f35d41f84737aee12a5e
MalgoskaG <ghouga.m@gmail.com>
2019-09-20 00:18:52 +0200

    add the option of fetching the tasks

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,19 @@
 import "./scss/main.scss"
 import "./startPageHandelers"
+import axios from 'axios'
+
+
+//adding new list//
+const addButton = document.querySelector('navbar__createNewListButton');
+const confirmButton = document.querySelector('.navbar__button');
+const listNameInput = document.querySelector('addNewListWindow__form--input')
+
+console.log(confirmButton)
+
+//addButton.addEventListener('click', funkcja pokazująca ramkę z dodawaniem listy);
+
+confirmButton.addEventListener('click', async (e) => {
+
+    const lists = await axios.get('http://localhost:3000/api/lists')
+    console.log(lists)
+})
--- server/app.js
+++ server/app.js
@@ -3,8 +3,11 @@ require('express-async-errors');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const routes = require('./routes/routes')(app);
 const config = require('./config');
+
+//CORS handling
+app.use(cors());
+require('./routes/routes')(app);
 
 mongoose.connect(config.db, {
     useNewUrlParser: true
@@ -12,8 +15,6 @@ mongoose.connect(config.db, {
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-//CORS handling
-app.use(cors());
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}...`));
