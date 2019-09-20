392a6488bd576691be02c1c987b5ed96ea399c9a
Weronika <riqiviki@gmail.com>
2019-09-20 12:26:50 +0200

    add get tasks and show tasks in DOM

--- client/index.js
+++ client/index.js
@@ -24,3 +24,44 @@ const listNameInput = document.querySelector('addNewListWindow__form--input')
 //     console.log(error)
 //     })
 // })
+
+//getting tasks
+const URL = '*';
+const getTasks = async () => {
+    try {
+        const res = await axios.get('${*}/tasks');
+        
+        const tasks = res.data;
+
+        return tasks;
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+//showing tasks
+const createLi = item => {
+    const li = document.createElement('li');
+
+    li.appendChild(document.createTextNode(item.title));
+
+    return li;
+};
+
+const addTasksToDOM = tasks => {
+    const ul = document.querySelector('ul');
+
+    if (Array.isArray(tasks) && tasks.length > 0) {
+        tasks.map(task => {
+            ul.appendChild(createLi(task));
+        });
+    } else if (tasks) {
+        ul.appendChild(createLi(tasks));
+    }
+};
+
+const main = async () => {
+    addTasksToDOM(await getTasks());
+};
+
+main ();
