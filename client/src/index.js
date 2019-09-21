4cd75ba9f4b39dd55c29cd4bab8ca14b21a60f99
Konrad C <konrad1988@interia.pl>
2019-09-21 22:22:59 +0200

    add list event handlers and all lists and tasks

--- client/src/index.js
+++ client/src/index.js
@@ -2,55 +2,6 @@ import "./scss/main.scss"
 import axios from 'axios'
 import {addTaskToDone} from './js/task-done';
 
-<<<<<<< HEAD
-//-----------------------------------------------------------------------------------------------------------------
-//  WYŚWIETLANIE LISTY Z LISTAMI
-
-// const addButton = document.querySelector('navbar__createNewListButton');
-// const confirmButton = document.querySelector('.navbar__button');
-// const listNameInput = document.querySelector('addNewListWindow__form--input')
-
-//console.log(confirmButton)
-// confirmButton.addEventListener('click', async (e) => {
-
-//     const lists = await axios.get('http://localhost:3000/api/lists')
-//     console.log(lists)
-// })
-
-//----------------------------------------------------------------------------------------------------------------
-//DODAWANIE NOWEJ LISTY
-
-// const addsButton = document.getElementById('addList');
-// const confirmsButton = document.querySelector('addNewListWindow__form--confirmButton');
-// const listNamesInput = document.querySelector('addNewListWindow__form--input')
-// const addNewList = document.getElementsByClassName('addNewListWindow')
-// const newListBackground = document.getElementsByClassName('newListBackground');
-
-// addsButton.onclick = function() {
-//     for (let i =0; i<addNewList.length; i+=1){
-//         addNewList[i].style.display = "block";
-//         newListBackground[0].style.background = 'rgba(255,255,255, 0.5)';
-        
-//     }
-// };
-
-// confirmButton.addEventListener('click', function(e) {
-
-//     axios.post('/lists', {
-//         userID: '', 
-//         name: listNameInput.value,
-//         createdAt: new Date().getDate,
-//         color: '',
-//         tasks: []
-//     })
-//     .then(function (response) {
-//     console.log(response)
-//     })
-//     .catch(function (error) {
-//     console.log(error)
-//     })
-// })
-=======
 const URL = 'http://localhost:3000/api';
 
 let id1 = '5d82b4527f81363ec8162055';
@@ -249,4 +200,3 @@ const mainTask = async () => {
 };
 
 //mainTask ();
->>>>>>> develop
