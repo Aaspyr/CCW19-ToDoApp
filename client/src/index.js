fea012fe32b34d54c0d2d87d07627ddbf145b533
Konrad C <konrad1988@interia.pl>
2019-09-25 00:57:09 +0200

    add remove list function again, uncomment code for showing lists and tasks

--- client/src/index.js
+++ client/src/index.js
@@ -2,21 +2,6 @@ import "./scss/main.scss"
 import axios from 'axios'
 import {addTaskToDone} from './js/task-done';
 
-
-//-----------------------------------------------------------------------------------------------------------------
-//  WYŚWIETLANIE LISTY Z LISTAMI
-
-// const addButton = document.querySelector('navbar__createNewListButton');
-// const confirmButton = document.querySelector('.navbar__button');
-// const listNameInput = document.querySelector('addNewListWindow__form--input')
-
-//console.log(confirmButton)
-// confirmButton.addEventListener('click', async (e) => {
-
-//     const lists = await axios.get('http://localhost:3000/api/lists')
-//     console.log(lists)
-// })
-
 //----------------------------------------------------------------------------------------------------------------
 //DODAWANIE NOWEJ LISTY
 
@@ -68,189 +53,214 @@ addTask.onclick = function() {
     console.log(addTaskWindow);
 }
 
-// let id1 = '5d82b4527f81363ec8162055';
-// let id2 = '5d85ad3c7d30410b4a4808dc';
-// let id3 = '5d8263c6d88e2138505385cd';
+ let id1 = '5d82b4527f81363ec8162055';
+ let id2 = '5d85ad3c7d30410b4a4808dc';
+ let id3 = '5d8263c6d88e2138505385cd';
 
 // //adding new list//
-// const addButton = document.querySelector('navbar__createNewListButton');
-// const confirmButton = document.querySelector('.navbar__button');
-// const listNameInput = document.querySelector('addNewListWindow__form--input')
+const addButton = document.querySelector('navbar__createNewListButton');
+const confirmButton = document.querySelector('.navbar__button');
+const listNameInput = document.querySelector('addNewListWindow__form--input')
 
-// const listDzis = document.getElementById('dzis');
-// const listSzkola = document.getElementById('szkola');
-// const listPraca = document.getElementById('praca');
-// const listDom = document.getElementById('dom');
-// const listTaski = document.getElementById('taski');
+const listDzis = document.getElementById('dzis');
+const listSzkola = document.getElementById('szkola');
+const listPraca = document.getElementById('praca');
+const listDom = document.getElementById('dom');
+const listTaski = document.getElementById('taski');
 
-// listDzis.addEventListener('click', async() => {
-//     addListsToDOM(await getTasks(id1))
-// })
-// listPraca.addEventListener('click', async() => {
-//     addListsToDOM(await getTasks(id2))
-// })
-// listSzkola.addEventListener('click', async() => {
-//     addListsToDOM(await getTasks(id3))
-// })
-// listDom.addEventListener('click', async () => {
-//     addListsToDOM(await mainList(''))})
-// listTaski.addEventListener('click', async () => {
-//     addListsToDOM(await getTasks(''))})
+listDzis.addEventListener('click', async() => {
+    addListsToDOM(await getTasks(id1))
+})
+listPraca.addEventListener('click', async() => {
+    addListsToDOM(await getTasks(id2))
+})
+listSzkola.addEventListener('click', async() => {
+    addListsToDOM(await getTasks(id3))
+})
+listDom.addEventListener('click', async () => {
+    addListsToDOM(await mainList(''))})
+listTaski.addEventListener('click', async () => {
+    addListsToDOM(await getTasks(''))})
 
-// confirmButton.addEventListener('click', async (e) => {
+confirmButton.addEventListener('click', async (e) => {
 
-//     //const lists = await axios.get(`${URL}/lists`)
-//     //console.log(lists)
-//     mainTask()
-// })
+    //const lists = await axios.get(`${URL}/lists`)
+    //console.log(lists)
+    mainTask()
+})
 
 
-// //getting lists
-// const getLists = async () => {
-//     try {
-//         const id = '';
-//         const res = await axios.get(`${URL}/lists/${id}`);
-//         const lists = res.data;
-//         console.log(lists)
-//         return lists;
-//     } catch (e) {
-//         console.error(e);
-//     }
-// };
-// //console.log('listy'+ getLists());
+//getting lists
+const getLists = async () => {
+    try {
+        const id = '';
+        const res = await axios.get(`${URL}/lists/${id}`);
+        const lists = res.data;
+        console.log(lists)
+        return lists;
+    } catch (e) {
+        console.error(e);
+    }
+};
+//console.log('listy'+ getLists());
 
-// //showing lists
-// const createLiList = item => {
+//showing lists
+const createLiList = item => {
 
-//     const li = document.createElement('li');
-//     const p1 = document.createElement('p');
-//     const p2 = document.createElement('p');
-//     const p3 = document.createElement('p');
-//     const btn1 = document.createElement('button');
-//     const btn2 = document.createElement('button');
+    const li = document.createElement('li');
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+    const p3 = document.createElement('p');
+    const btn1 = document.createElement('button');
+    const btn2 = document.createElement('button');
 
-//     btn1.classList.add('undoneContainer__taskList--edit', 'edit-task');
-//     btn2.classList.add('undoneContainer__taskList--delete');
-//     btn1.innerHTML ='<img src="src/img/edit.svg" alt="">';
-//     btn2.innerHTML ='<img delete="true" src="src/img/garbage.svg" alt="">';
+    btn1.classList.add('undoneContainer__taskList--edit', 'edit-task');
+    btn2.classList.add('undoneContainer__taskList--delete');
+    btn1.innerHTML ='<img src="src/img/edit.svg" alt="">';
+    btn2.innerHTML ='<img deletebarlist="true" src="src/img/garbage.svg" alt="">';
     
-//     p1.appendChild(document.createTextNode(item.name));
-//     p3.appendChild(document.createTextNode(item.tasks));
-//     p2.appendChild(document.createTextNode(item.userId.name));
+    p1.appendChild(document.createTextNode(item.name));
+    p3.appendChild(document.createTextNode(item.tasks));
+    p2.appendChild(document.createTextNode(item.userId.name));
 
-//     li.id = item._id;
-//     li.appendChild(p1);
-//     li.appendChild(p3);
-//     li.appendChild(p2);
-//     li.appendChild(btn1);
-//     li.appendChild(btn2);
+    li.id = item._id;
+    li.appendChild(p1);
+    li.appendChild(p3);
+    li.appendChild(p2);
+    li.appendChild(btn1);
+    li.appendChild(btn2);
     
-//     if (item.done === true){
-//         li.classList.add("doneContainer__taskList--element");
-//     } else {
-//         li.classList.add("undoneContainer__taskList--element");
-//     }
-//     return li;
-// };
+    if (item.done === true){
+        li.classList.add("doneContainer__taskList--element");
+    } else {
+        li.classList.add("undoneContainer__taskList--element");
+    }
+    // Remove LI on click
+    li.onclick = async e => await removeList(e, li);
+    return li;
+};
 
-// const addListsToDOM = lists => {
-//     const done = document.querySelector('.doneContainer__taskList');
-//     const undone = document.querySelector('.undoneContainer__taskList');
+const addListsToDOM = lists => {
+    const done = document.querySelector('.doneContainer__taskList');
+    const undone = document.querySelector('.undoneContainer__taskList');
     
-//     if (Array.isArray(lists) && lists.length > 0) {
-//         lists.map(list => {
-//             if(list.done===true){
-//                 console.log(list);
-//                 done.appendChild(createLiList(list));
-//             } else {
-//                 undone.appendChild(createLiList(list));
-//             }   
-//         });
-//     } else if (lists) {
-//         ul.appendChild(createLiList(lists));
-//     }
+    if (Array.isArray(lists) && lists.length > 0) {
+        lists.map(list => {
+            if(list.done===true){
+                console.log(list);
+                done.appendChild(createLiList(list));
+            } else {
+                undone.appendChild(createLiList(list));
+            }   
+        });
+    } else if (lists) {
+        ul.appendChild(createLiList(lists));
+    }
 
-//     editTask("update possibilty 2")
-// };
+    editTask("update possibilty 2")
+};
 
-// const mainList = async () => {
-//     addListsToDOM(await getLists());
-// };
+const mainList = async () => {
+    addListsToDOM(await getLists());
+};
 
-// //mainList ();
+//mainList ();
 
-// //getting tasks
-// const getTasks = async (id) => {
-//     try {
-//         //const id = '';
-//         const res = await axios.get(`${URL}/tasks/${id}`);
-//         const tasks = res.data;
-//         return tasks;
-//     } catch (e) {
-//         console.error(e);
-//     }
-// };
+//getting tasks
+const getTasks = async (id) => {
+    try {
+        //const id = '';
+        const res = await axios.get(`${URL}/tasks/${id}`);
+        const tasks = res.data;
+        return tasks;
+    } catch (e) {
+        console.error(e);
+    }
+};
 
-// //showing tasks
-// const createLi = item => {
+//showing tasks
+const createLi = item => {
 
-//     const li = document.createElement('li');
-//     const p1 = document.createElement('p');
-//     const p2 = document.createElement('p');
-//     const btn1 = document.createElement('button');
-//     const btn2 = document.createElement('button');
+    const li = document.createElement('li');
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+    const btn1 = document.createElement('button');
+    const btn2 = document.createElement('button');
 
-//     btn1.classList.add('undoneContainer__taskList--edit');
-//     btn2.classList.add('undoneContainer__taskList--done');
-//     btn1.innerHTML ='<img src="src/img/edit.svg" alt="">';
-//     btn2.innerHTML ='<img src="src/img/tick.svg" alt="">';
+    btn1.classList.add('undoneContainer__taskList--edit');
+    btn2.classList.add('undoneContainer__taskList--done');
+    btn1.innerHTML ='<img src="src/img/edit.svg" alt="">';
+    btn2.innerHTML ='<img src="src/img/tick.svg" alt="">';
     
-//     p1.appendChild(document.createTextNode(item.name));
-//     p2.appendChild(document.createTextNode(item.done));
+    p1.appendChild(document.createTextNode(item.name));
+    p2.appendChild(document.createTextNode(item.done));
 
-//     li.appendChild(p1);
-//     li.appendChild(p2);
-//     li.appendChild(btn1);
-//     li.appendChild(btn2);
+    li.appendChild(p1);
+    li.appendChild(p2);
+    li.appendChild(btn1);
+    li.appendChild(btn2);
     
-//     if (item.done === true){
-//         li.classList.add("doneContainer__taskList--element");
-//     } else {
-//         li.classList.add("undoneContainer__taskList--element");
-//     }
-//     return li;
-// };
+    if (item.done === true){
+        li.classList.add("doneContainer__taskList--element");
+    } else {
+        li.classList.add("undoneContainer__taskList--element");
+    }
+    // Remove LI on click
+    li.onclick = async e => await removeList(e, li);
+    return li;
+};
 
-// const addTasksToDOM = tasks => {
-//     const done = document.querySelector('.doneContainer__taskList');
-//     const undone = document.querySelector('.undoneContainer__taskList');
+const addTasksToDOM = tasks => {
+    const done = document.querySelector('.doneContainer__taskList');
+    const undone = document.querySelector('.undoneContainer__taskList');
     
-//     if (Array.isArray(tasks) && tasks.length > 0) {
-//         tasks.map(task => {
-//             if(task.done===true){
-//                 console.log(task);
-//                 done.appendChild(createLi(task));
-//             } else {
-//                 undone.appendChild(createLi(task));
-//             }   
-//         });
-//     } else if (tasks) {
-//         ul.appendChild(createLi(tasks));
-//     }
-// };
+    if (Array.isArray(tasks) && tasks.length > 0) {
+        tasks.map(task => {
+            if(task.done===true){
+                console.log(task);
+                done.appendChild(createLi(task));
+            } else {
+                undone.appendChild(createLi(task));
+            }   
+        });
+    } else if (tasks) {
+        ul.appendChild(createLi(tasks));
+    }
+};
 
-// const mainTask = async () => {
-//     addTasksToDOM(await getTasks());
-// };
+const mainTask = async () => {
+    addTasksToDOM(await getTasks());
+};
 
-// const editTask = (name) => {
-//     document.querySelectorAll('.edit-task').forEach(btn => btn.addEventListener('click', async (e) => {
-//         const taskId = e.currentTarget.parentNode.id;
-//         const list = e.currentTarget.parentNode;
-//         const edit = await axios.put(`${URL}/tasks/${taskId}`, {name}); 
-//         list.children[0].innerText = edit.data.name;
-//     }));
-// }
+const editTask = (name) => {
+    document.querySelectorAll('.edit-task').forEach(btn => btn.addEventListener('click', async (e) => {
+        const taskId = e.currentTarget.parentNode.id;
+        const list = e.currentTarget.parentNode;
+        const edit = await axios.put(`${URL}/tasks/${taskId}`, {name}); 
+        list.children[0].innerText = edit.data.name;
+    }));
+}
 
-//mainTask ();
+mainTask();
 
+// Delete list
+
+const deleteList = async (lid) => {
+    console.log(`id z delete ${lid}`)
+    try {
+      const res = await axios.delete(`${URL}/lists/${lid}`);
+      console.log(`Deleted list ID: `, lid);
+  
+      return res.data;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const removeList = async (e, li) => {
+    const lid = li.id;
+    if(e.target.getAttribute('deletebarlist')=='true'){
+        e.target.parentElement.parentElement.parentElement.removeChild(li);
+    }
+    await deleteList(lid);
+  };
